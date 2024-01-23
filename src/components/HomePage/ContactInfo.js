3aac6e57fd34281811c4a33cd3266ac4b6081b7d
Mar Mar Soe <marmarsoe@Mars-MacBook-Air.local>
2024-01-23 19:14:36 +0900

    update github aciton

--- src/components/HomePage/ContactInfo.js
+++ src/components/HomePage/ContactInfo.js
@@ -6,11 +6,11 @@ const ContactInfo = () => {
       </div>
       <div className="flex flex-row justify-between w-[1000px] mt-10 ml-[250px]">
         <div className="flex flex-col items-start text-slate-700">
-          <a href="">Facebook</a>
-          <a href="">Instagram</a>
-          <a href="">Line</a>
-          <a href="">LinkedIn</a>
-          <a href="">GitHub</a>
+          <a href="/">Facebook</a>
+          <a href="/">Instagram</a>
+          <a href="/">Line</a>
+          <a href="/">LinkedIn</a>
+          <a href="/">GitHub</a>
         </div>
         <div className="flex flex-col justify-center items-center w-[600px] bg-orange-200">
           <h2>Contact Me</h2>
