eac17be2e42bd1e682a9f41948b5b4d9db4f9f28
Mar Mar Soe <marmarsoe@isgm2.com>
2024-01-29 15:50:44 +0900

    refactor: update projects section in home page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () =>{
   return(<div className="container mx-auto mt-20">
     <div className="flex justify-center items-center  self-center h-28">
-        <div className="font-light">Designed & Built By <span className="text-orange-300">Me</span></div>
+        <div className="font-light">Designed & Built By <span className="text-orange-500">Me</span></div>
     </div>
   </div>);
 }
--- src/components/HomePage/Projects.js
+++ src/components/HomePage/Projects.js
@@ -9,119 +9,115 @@ const Projects = () => {
         Project
       </div>
       <div className="flex flex-col gap-y-5">
-      <div className="flex flex-row items-center mt-5 gap-x-8">
-        <div className="flex items-start">
-          <img
-            src={Pokemon}
-            alt="my-profile"
-            className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
-          />
-        </div>
-        <div className="flex flex-col justify-start items-start">
-          <div className="font-bold text-orange-600 text-lg">PokeDev</div>
-          <div className="flex flex-row gap-x-2 text-grey-500 font-light">
-            <div>React</div>
-            <div>Tailwindcss</div>
+        <div className="flex flex-row items-center mt-5 gap-x-8">
+          <div className="flex items-start">
+            <img
+              src={Pokemon}
+              alt="my-profile"
+              className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
+            />
           </div>
-          <div className="text-wrap w-[700px]">
-            This project is a practice project that was used React JS,
-            Tailwindcss, Pokemon API. React hooks (useState, useContext,
-            createContext) were used.
-          </div>
-          <div className="mt-3 flex flex-row gap-x-2">
-            <Button
-              variant="outlined"
-              href="https://snazzy-croissant-7c932a.netlify.app/"
-              color="warning"
-              sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
-            >
-              Live
-            </Button>
+          <div className="flex flex-col justify-start items-start">
+            <div className="font-bold text-orange-600 text-lg">PokeDev</div>
+            <div className="flex flex-row gap-x-2 text-grey-500 font-light">
+              <div>React</div>
+              <div>Tailwindcss</div>
+            </div>
+            <div className="text-wrap w-[700px]">
+              This project is a practice project that was used React JS,
+              Tailwindcss, Pokemon API. React hooks (useState, useContext,
+              createContext) were used.
+            </div>
+            <div className="mt-3 flex flex-row gap-x-2">
+              <Button
+                variant="outlined"
+                href="https://snazzy-croissant-7c932a.netlify.app/"
+                color="warning"
+                sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
+              >
+                Live
+              </Button>
 
-            <Button
-              variant="outlined"
-              href="https://github.com/MarMarSoe/pokemon-with-react"
-              color="warning"
-              sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
-            >
-              GitHub
-            </Button>
+              <Button
+                variant="outlined"
+                href="https://github.com/MarMarSoe/pokemon-with-react"
+                color="warning"
+                sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
+              >
+                GitHub
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Project II */}
-      <div className="flex flex-row items-center mt-5 gap-x-8">
-        <div className="flex items-start">
-          <img
-            src={Portfolio}
-            alt="my-profile"
-            className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
-          />
-        </div>
-        <div className="flex flex-col justify-start items-start">
-          <div className="font-bold text-orange-600 text-lg">My Portfolio</div>
-          <div className="flex flex-row gap-x-2 text-grey-500 font-light">
-            <div>React</div>
-            <div>Tailwindcss</div>
-            <div>Material UI</div>
+        {/* Project II */}
+        <div className="flex flex-row items-center mt-5 gap-x-8">
+          <div className="flex items-start">
+            <img
+              src={Portfolio}
+              alt="my-profile"
+              className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
+            />
           </div>
-          <div className="text-wrap w-[700px]">
-            This project is a practice project that was used React JS,
-            Tailwindcss, Pokemon API. React hooks (useState, useContext,
-            createContext) were used.
-          </div>
-          <div className="mt-3 flex flex-row gap-x-2">
-            <Button
-              variant="outlined"
-              href="https://effortless-treacle-0224e7.netlify.app/"
-              color="warning"
-              sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
-            >
-              Live
-            </Button>
-
-            <Button
-              variant="outlined"
-              href="https://github.com/MarMarSoe/MarMarSoe.github.io"
-              color="warning"
-              sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
-            >
-              GitHub
-            </Button>
+          <div className="flex flex-col justify-start items-start">
+            <div className="font-bold text-orange-600 text-lg">
+              My Portfolio
+              <span className="inline-flex items-center rounded-md bg-orange-300 px-2 py-1 text-xs font-medium text-gray-600 ring-2 ring-inset ring-gray-500/10 ml-2">
+                This site
+              </span>
+            </div>
+            <div className="flex flex-row gap-x-2 text-grey-500 font-light">
+              <div>React</div>
+              <div>Tailwindcss</div>
+              <div>Material UI</div>
+            </div>
+            <div className="text-wrap w-[700px]">
+              My portfolio website, you're probably looking at right now.
+            </div>
+            <div className="mt-3 flex flex-row gap-x-2">
+              <Button
+                variant="outlined"
+                href="https://github.com/MarMarSoe/MarMarSoe.github.io"
+                color="warning"
+                sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
+              >
+                GitHub
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Project III */}
-      <div className="flex flex-row items-center mt-5 gap-x-8">
-        <div className="flex items-start">
-          <img
-            src={Portfolio}
-            alt="my-profile"
-            className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
-          />
+        {/* Project III */}
+        <div className="flex flex-row items-center mt-5 gap-x-8">
+          <div className="flex items-start">
+            <img
+              src={Portfolio}
+              alt="my-profile"
+              className="w-[220px] h-[180px] border hover:border-orange-300 hover:scale-105 transition duration-300"
+            />
+          </div>
+          <div className="flex flex-col justify-start items-start">
+            <div className="font-bold text-orange-600 text-lg">
+              UI/UX Design Project
+            </div>
+            <div className="flex flex-row gap-x-2 text-grey-500 font-light">
+              <div>Figma</div>
+            </div>
+            <div className="text-wrap w-[700px]">
+              This project is a practice project that was used React JS,
+              Tailwindcss, Pokemon API. React hooks (useState, useContext,
+              createContext) were used.
+            </div>
+            <div className="mt-3 flex flex-row gap-x-2">
+              <Button
+                variant="outlined"
+                href="https://www.figma.com/file/Ep5gp13K6gqi0UBwwC8MrC/MMS-Portfolio?type=design&node-id=1%3A2&mode=design&t=YtzjW82BEljiga2Q-1"
+                color="warning"
+                sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
+              >
+                Link
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col justify-start items-start">
-          <div className="font-bold text-orange-600 text-lg">UI/UX Design Project</div>
-          <div className="flex flex-row gap-x-2 text-grey-500 font-light">
-            <div>Figma</div>
-          </div>
-          <div className="text-wrap w-[700px]">
-            This project is a practice project that was used React JS,
-            Tailwindcss, Pokemon API. React hooks (useState, useContext,
-            createContext) were used.
-          </div>
-          <div className="mt-3 flex flex-row gap-x-2">
-            <Button
-              variant="outlined"
-              href="https://www.figma.com/file/Ep5gp13K6gqi0UBwwC8MrC/MMS-Portfolio?type=design&node-id=1%3A2&mode=design&t=YtzjW82BEljiga2Q-1"
-              color="warning"
-              sx={{ fontSize: 16, textTransform: "capitalize", width: 100 }}
-            >
-              Link
-            </Button>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );
